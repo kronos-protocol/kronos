cd1722145a3f33e7396e12dda29dd952b4fb7caa
Saveli Gulas <saveli.gulas@gmail.com>
2025-06-18 02:14:36 +0200

    added empty frame_lifecycle source file and updated name for internal header with _ prefix for clearer differentiation of public and private APIs/Header files

---
+++ src/frame/frame_lifecycle.c
@@ -0,0 +1,15 @@
+#include <../internal/_kronos_internal.h>
+#include <stdint.h>
+#include <stdio.h>
+
+Frame krs_frame_create(char* buffer) {
+
+}
+
+Frame* krs_frame_create_heap(char* buffer) {
+
+}
+
+void krs_frame_init(char* buffer, Frame* out) {
+
+}
