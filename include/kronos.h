f98d3d9a0000e95ef4dfe543f81ec0e6f91541dd
Saveli Gulas <saveli.gulas@gmail.com>
2025-06-18 02:12:15 +0200

    added Frame typedef and Frame creation functions in kronos API

--- include/kronos.h
+++ include/kronos.h
@@ -1,4 +1,11 @@
 #ifndef KRONOS_H
 #define KRONOS_H
 
+typedef struct Frame Frame;
+
+Frame krs_frame_create(char* buffer);
+Frame* krs_frame_create_heap(char* buffer);
+void krs_frame_init(char* buffer, Frame* out);
+
+
 #endif //KRONOS_H
