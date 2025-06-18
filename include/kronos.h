effde5bb7711972a9593d1bc4b8533fd3b504c65
Saveli Gulas <saveli.gulas@gmail.com>
2025-06-19 01:56:35 +0200

    updated API creation functions to use uint8_t buffers insted of char buffers and added received_bytes field

--- include/kronos.h
+++ include/kronos.h
@@ -1,11 +1,12 @@
 #ifndef KRONOS_H
 #define KRONOS_H
+#include <stdint.h>
 
 typedef struct Frame Frame;
 
-Frame krs_frame_create(char* buffer);
-Frame* krs_frame_create_heap(char* buffer);
-void krs_frame_init(char* buffer, Frame* out);
+Frame krs_frame_create(uint8_t* buffer, uint16_t received_bytes);
+Frame* krs_frame_create_heap(uint8_t* buffer, uint16_t received_bytes);
+void krs_frame_init(uint8_t* buffer, uint16_t received_bytes, Frame* out);
 
 
 #endif //KRONOS_H
