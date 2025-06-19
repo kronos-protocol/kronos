4d6a2f809bb989e30f04de8e2363adb7da251940
Saveli Gulas <saveli.gulas@gmail.com>
2025-06-19 14:57:31 +0200

    added static function to create frames

--- src/frame/frame_lifecycle.c
+++ src/frame/frame_lifecycle.c
@@ -1,15 +1,20 @@
-#include <../internal/_kronos_internal.h>
+#include <../include/kronos.h>
+#include <../internal/kronos_internal.h>
 #include <stdint.h>
-#include <stdio.h>
+#include <string.h>
 
-Frame krs_frame_create(char* buffer) {
-
+static Frame s_create_frame(const uint8_t* buffer, const uint16_t received_bytes, uint8_t* frame_data_out) {
+    Frame frame;
+    frame.length = received_bytes;
+    frame.protocol_char = (char)buffer[0];
+    frame.protocol_version = buffer[1];
+    frame.frame_type = buffer[2];
+    frame.presence_flags = ((uint16_t)buffer[3] << 8) | buffer[4];
+    frame.data = frame_data_out;
+    memcpy(frame_data_out, buffer + KRONOS_FRAME_HEADER_LENGTH, received_bytes - KRONOS_FRAME_HEADER_LENGTH);
+    return frame;
 }
 
-Frame* krs_frame_create_heap(char* buffer) {
-
-}
-
-void krs_frame_init(char* buffer, Frame* out) {
-
+Frame krs_frame_create(const uint8_t* buffer, uint8_t* stack_data_out, uint16_t received_bytes) {
+    return s_create_frame(buffer, received_bytes, stack_data_out);
 }
