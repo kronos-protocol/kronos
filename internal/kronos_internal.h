7227f4496ba3f249b01c58f81b3207b6a593189a
Saveli Gulas <saveli.gulas@gmail.com>
2025-06-19 14:58:28 +0200

    reformatting

--- internal/kronos_internal.h
+++ internal/kronos_internal.h
@@ -7,10 +7,11 @@ struct Frame {
 
     // Data in Char Buffer
     // Header
-    char protocol_char; // must be 0x4B ASCII 'K'
+    char protocol_char;       // must be 0x4B ASCII 'K'
     uint8_t protocol_version; // bitmask: major(3bit) + minor(3bit) + patch(2bit)
     uint8_t frame_type;
-    uint16_t presence_flags; // bitmask for flags, each 1bit: frame_id, session_id, sender_id, fragment_info, (Rest Empty for now)
+    uint16_t presence_flags; // bitmask for flags, each 1bit: frame_id, session_id,
+                             // sender_id, fragment_info, (Rest Empty for now)
 
     // Body
     uint8_t* data;
@@ -18,4 +19,4 @@ struct Frame {
 
 #define KRONOS_FRAME_HEADER_LENGTH 5
 
-#endif //KRONOS_INTERNAL_H
+#endif // KRONOS_INTERNAL_H
