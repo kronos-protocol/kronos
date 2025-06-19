88b0e3f60d1a99af737c3e508241daca14477f34
Saveli Gulas <saveli.gulas@gmail.com>
2025-06-19 14:58:22 +0200

    reformatting and added kronos_math.h API and bitmask.c

--- include/kronos.h
+++ include/kronos.h
@@ -9,5 +9,4 @@ Frame krs_frame_create(const uint8_t* buffer, uint8_t* stack_data_out, uint16_t 
 Frame* krs_frame_create_heap(uint8_t* buffer, uint16_t received_bytes);
 void krs_frame_init(uint8_t* buffer, uint16_t received_bytes, Frame* out);
 
-
-#endif //KRONOS_H
+#endif // KRONOS_H
---
+++ src/math/kronos_math.h
@@ -0,0 +1,8 @@
+#ifndef KRONOS_MATH_H
+#define KRONOS_MATH_H
+#include <stdbool.h>
+#include <stdint.h>
+
+bool ikrs_math_bitmask_get_flag(uint8_t flag_pos, uint8_t* data, uint8_t length);
+
+#endif //KRONOS_MATH_H
