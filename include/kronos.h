ab63883e4120246254265a2a56da20ed638ae32a
saveligulas <saveli.gulas@gmail.com>
2025-06-20 18:38:10 +0200

    updated kronos API functions to have a parameter defining the data size if defined by the caller to ensure data is not written out of bounds

--- include/kronos.h
+++ include/kronos.h
@@ -5,8 +5,8 @@
 
 typedef struct Frame Frame;
 
-Frame krs_frame_create(const uint8_t* buffer, uint8_t* stack_data_out, uint16_t received_bytes);
-Frame* krs_frame_create_heap(uint8_t* buffer, uint16_t received_bytes);
-void krs_frame_init(uint8_t* buffer, uint16_t received_bytes, Frame* out);
+Frame krs_frame_create(const uint8_t* buffer, uint16_t received_bytes, uint8_t* stack_data_out, uint16_t stack_data_out_size);
+Frame* krs_frame_create_heap(const uint8_t* buffer, uint16_t received_bytes);
+void krs_frame_init(const uint8_t* buffer, uint16_t received_bytes, Frame* out, uint16_t out_data_size);
 
 #endif // KRONOS_H
