c205c49bb14cb213900d0690256d91c4b2f47c3f
saveligulas <saveli.gulas@gmail.com>
2025-06-20 21:18:28 +0200

    reverted change in krs_math_bitmask_get_flag to not use htonl because only 8 bit integers are used which always are Big Endian and added implementation to set flags

--- src/math/bitmask.c
+++ src/math/bitmask.c
@@ -7,10 +7,19 @@ bool krs_math_bitmask_get_flag(const uint8_t flag_pos, const uint8_t* data, cons
         return false;
     }
 
-    uint8_t big_endian_flag_pos = htonl(flag_pos);
-
-    uint8_t array_pos = big_endian_flag_pos / 8;
-    uint8_t bit_pos = big_endian_flag_pos % 8;
+    uint8_t array_pos = flag_pos / 8;
+    uint8_t bit_pos = flag_pos % 8;
 
     return (data[array_pos] & (1 << (7 - bit_pos))) != 0; // Orders from left to right with Big Endian
+}
+
+void krs_math_bitmask_set_flag(uint8_t flag_pos, uint8_t* data, uint8_t length) {
+    if (flag_pos >= length * 8) {
+        return;
+    }
+
+    uint8_t array_pos = flag_pos / 8;
+    uint8_t bit_pos = flag_pos % 8;
+
+    data[array_pos] |= (1 << (7 - bit_pos));
 }
