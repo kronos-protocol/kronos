cb64e8b582cb8c52e9dfec0d2e12f5959a046a2a
Saveli Gulas <saveli.gulas@gmail.com>
2025-06-26 17:15:12 +0200

    added FrameType Enum to kronos API

--- include/kronos.h
+++ include/kronos.h
@@ -4,11 +4,23 @@
 #include <stdint.h>
 
 typedef struct Frame Frame;
+typedef enum FrameType FrameType;
 
 Frame krs_frame_create(const uint8_t* buffer, uint16_t received_bytes, uint8_t* stack_data_out, uint16_t stack_data_out_size);
 Frame* krs_frame_create_heap(const uint8_t* buffer, uint16_t received_bytes);
 void krs_frame_init(const uint8_t* buffer, uint16_t received_bytes, Frame* out, uint16_t out_data_size);
 
 void krs_frame_destroy(Frame** frame);
+
+enum FrameType {
+    // Client-Only
+    CONNECTION = 10,
+    HEARTBEAT = 11,
+
+
+    // Server-Only
+    CONNECTION_ACK,
+
+};
 
 #endif // KRONOS_H
