16dcebf3b6e1a416bee918900606bde6b939ac53
Saveli Gulas <saveli.gulas@gmail.com>
2025-06-29 11:29:26 +0200

    added frame types

--- include/kronos.h
+++ include/kronos.h
@@ -10,17 +10,25 @@ Frame krs_frame_create(const uint8_t* buffer, uint16_t received_bytes, uint8_t* 
 Frame* krs_frame_create_heap(const uint8_t* buffer, uint16_t received_bytes);
 void krs_frame_init(const uint8_t* buffer, uint16_t received_bytes, Frame* out, uint16_t out_data_size);
 
+
+
 void krs_frame_destroy(Frame** frame);
 
 enum FrameType {
+    // General
+    MESSAGE_ACK = 0,
+    BASIC_MESSAGE = 1,
+
+
+
     // Client-Only
     CONNECTION = 10,
     HEARTBEAT = 11,
+    SOCKET_SETUP = 12,
 
 
     // Server-Only
-    CONNECTION_ACK,
-
+    SOCKET_ACK = 22,
 };
 
 #endif // KRONOS_H
