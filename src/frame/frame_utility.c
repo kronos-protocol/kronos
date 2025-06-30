b5efea3c6d0bfe97d7c3a43ceb265f054395b2a3
saveligulas <saveli.gulas@gmail.com>
2025-06-30 15:50:31 +0200

    added function to get the length of the metadata in the body

--- src/frame/frame_utility.c
+++ src/frame/frame_utility.c
@@ -1,6 +1,16 @@
 #include "kronos.h"
+#include "../internal/frame_body.h"
+#include "../internal/frame_metadata.h"
 
-
-uint16_t _krs_frame_calculate_data_length(const uint16_t received_bytes) {
+uint16_t _krs_frame_calculate_body_length(const uint16_t received_bytes) {
     return received_bytes - KRONOS_FRAME_HEADER_LENGTH;
+}
+
+uint16_t krs_frame_body_metadata_get_length(const Frame* frame) {
+    uint16_t total_length = 0;
+    MetadataFlagPosition position = FLAG_COUNT;
+    for (int i = 0; i < position; i++) {
+        total_length+=KRS_METADATA_FLAG_POSITION_SIZE[i];
+    }
+    return total_length;
 }
