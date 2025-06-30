563264d40caed7ef320ea643cd12c544f436fb7d
saveligulas <saveli.gulas@gmail.com>
2025-06-30 15:49:54 +0200

    renamed data to body

--- src/frame/frame_lifecycle.c
+++ src/frame/frame_lifecycle.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 static Frame s_create_frame(const uint8_t* buffer, const uint16_t received_bytes, uint8_t* frame_data_out, uint16_t stack_data_out_size) {
-    uint16_t data_length = _krs_frame_calculate_data_length(received_bytes);
+    uint16_t data_length = _krs_frame_calculate_body_length(received_bytes);
     if (stack_data_out_size > data_length) {
         // TODO: use errno
         const Frame invalid = {0};
@@ -23,13 +23,14 @@ static Frame s_create_frame(const uint8_t* buffer, const uint16_t received_bytes
     }
 
     Frame frame;
-    frame.length = received_bytes;
+    frame.received_bytes = received_bytes;
     frame.protocol_char = (char)buffer[0];
     frame.protocol_version = buffer[1];
     frame.frame_type = buffer[2];
     frame.presence_flags = ((uint16_t)buffer[3] << 8) | buffer[4];
-    frame.data = frame_data_out;
-    memcpy(frame_data_out, buffer + KRONOS_FRAME_HEADER_LENGTH, _krs_frame_calculate_data_length(received_bytes));
+    frame.body = frame_data_out;
+    memcpy(frame_data_out, buffer + KRONOS_FRAME_HEADER_LENGTH, _krs_frame_calculate_body_length(received_bytes));
+    //TODO: initialize channel and packet_id
     return frame;
 }
 
@@ -39,20 +40,20 @@ Frame krs_frame_create(const uint8_t* buffer, const uint16_t received_bytes, uin
 
 Frame* krs_frame_create_heap(const uint8_t* buffer, const uint16_t received_bytes) {
     Frame* frame = malloc(sizeof(Frame));
-    uint16_t frame_data_size = _krs_frame_calculate_data_length(received_bytes);
-    frame->data = malloc(frame_data_size);
-    *frame = s_create_frame(buffer, received_bytes, frame->data, frame_data_size);
+    uint16_t frame_data_size = _krs_frame_calculate_body_length(received_bytes);
+    frame->body = malloc(frame_data_size);
+    *frame = s_create_frame(buffer, received_bytes, frame->body, frame_data_size);
     return frame;
 }
 
 
 void krs_frame_init(const uint8_t* buffer, const uint16_t received_bytes, Frame* out, const uint16_t out_data_size) {
-    *out = s_create_frame(buffer, received_bytes, out->data, out_data_size);
+    *out = s_create_frame(buffer, received_bytes, out->body, out_data_size);
 }
 
 void krs_frame_destroy(Frame** frame) {
     if (*frame != NULL) {
-        free((*frame)->data);
+        free((*frame)->body);
         free(*frame);
         *frame = NULL;
     }
