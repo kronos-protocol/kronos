3c596407b0a3eb5f4e8381a1c3602f090c5ec6ad
Saveli Gulas <saveli.gulas@gmail.com>
2025-07-02 11:29:32 +0200

    function rename

--- src/frame/frame_lifecycle.c
+++ src/frame/frame_lifecycle.c
@@ -1,3 +1,6 @@
+#include "kronos_math.h"
+
+
 #include <../include/kronos.h>
 #include <../internal/kronos_internal.h>
 #include <stdint.h>
@@ -5,8 +8,8 @@
 #include <string.h>
 
 static Frame s_create_frame(const uint8_t* buffer, const uint16_t received_bytes, uint8_t* frame_data_out, uint16_t stack_data_out_size) {
-    uint16_t data_length = _krs_frame_calculate_body_length(received_bytes);
-    if (stack_data_out_size > data_length) {
+    uint16_t data_length = krs_frame_calculate_body_length(received_bytes);
+    if (stack_data_out_size < data_length) {
         // TODO: use errno
         const Frame invalid = {0};
         return invalid;
@@ -23,14 +26,16 @@ static Frame s_create_frame(const uint8_t* buffer, const uint16_t received_bytes
     }
 
     Frame frame;
-    frame.received_bytes = received_bytes;
+    frame.body_length = krs_frame_calculate_body_length(received_bytes);
     frame.protocol_char = (char)buffer[0];
     frame.protocol_version = buffer[1];
-    frame.frame_type = buffer[2];
-    frame.presence_flags = ((uint16_t)buffer[3] << 8) | buffer[4];
+    frame.channel = buffer[2];
+    frame.frame_type = buffer[3];
+    frame.presence_flags = krs_math_uint16_from_uint8(buffer[4], buffer[5]);
+    frame.packet_id = krs_math_uint64_from_uint8(buffer, 6, received_bytes);
+    frame.body_length = data_length;
     frame.body = frame_data_out;
-    memcpy(frame_data_out, buffer + KRONOS_FRAME_HEADER_LENGTH, _krs_frame_calculate_body_length(received_bytes));
-    //TODO: initialize channel and packet_id
+    memcpy(frame_data_out, buffer + KRONOS_FRAME_HEADER_LENGTH, data_length);
     return frame;
 }
 
@@ -40,7 +45,7 @@ Frame krs_frame_create(const uint8_t* buffer, const uint16_t received_bytes, uin
 
 Frame* krs_frame_create_heap(const uint8_t* buffer, const uint16_t received_bytes) {
     Frame* frame = malloc(sizeof(Frame));
-    uint16_t frame_data_size = _krs_frame_calculate_body_length(received_bytes);
+    uint16_t frame_data_size = krs_frame_calculate_body_length(received_bytes);
     frame->body = malloc(frame_data_size);
     *frame = s_create_frame(buffer, received_bytes, frame->body, frame_data_size);
     return frame;
