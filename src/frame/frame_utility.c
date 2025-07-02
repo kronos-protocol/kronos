35235570bdf9fa88ea4cd2effb93e2a4a375b6bd
Saveli Gulas <saveli.gulas@gmail.com>
2025-07-02 11:29:21 +0200

    function rename

--- src/frame/frame_utility.c
+++ src/frame/frame_utility.c
@@ -2,7 +2,7 @@
 #include "../internal/frame_body.h"
 #include "../internal/frame_metadata.h"
 
-uint16_t _krs_frame_calculate_body_length(const uint16_t received_bytes) {
+uint16_t krs_frame_calculate_body_length(const uint16_t received_bytes) {
     return received_bytes - KRONOS_FRAME_HEADER_LENGTH;
 }
 
