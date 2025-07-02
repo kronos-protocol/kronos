8051f59bb664b7b628bea43b8402e225f8b82d52
Saveli Gulas <saveli.gulas@gmail.com>
2025-07-02 11:04:58 +0200

    fixed typo in function name

--- internal/frame_body.h
+++ internal/frame_body.h
@@ -4,6 +4,6 @@
 #include <stdint.h>
 
 uint16_t krs_frame_body_metadata_get_length(const Frame* frame);
-uint16_t krs_frame_body_body_get_length(const Frame* frame);
+uint16_t krs_frame_body_get_length(const Frame* frame);
 
 #endif //FRAME_CONTENT_H
