440e69470ce03f38ab78fd263e44d5049833b1f0
Saveli Gulas <saveli.gulas@gmail.com>
2025-07-06 13:43:17 +0200

    fixed naming to conventions

--- internal/frame_body.h
+++ internal/frame_body.h
@@ -3,7 +3,7 @@
 #include "kronos.h"
 #include <stdint.h>
 
-uint16_t krs_frame_body_metadata_get_length(const Frame* frame);
-uint16_t krs_frame_body_get_length(const Frame* frame);
+uint16_t krs_frame_body_metadata_get_length(const Frame_t* frame);
+uint16_t krs_frame_body_get_length(const Frame_t* frame);
 
 #endif //FRAME_CONTENT_H
