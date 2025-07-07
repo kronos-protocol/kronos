cc67e3036ba868a55ea0eb25a5eca0e511bfe60e
saveligulas <saveli.gulas@gmail.com>
2025-07-07 23:51:51 +0200

    typedef for EndpointManager and endpoint_setup function

--- include/kronos_server.h
+++ include/kronos_server.h
@@ -1,6 +1,10 @@
 #ifndef KRONOS_SERVER_H
 #define KRONOS_SERVER_H
+#include "kronos_network.h"
 
+typedef struct EndpointManager EndpointManager_t;
 typedef struct ClientConnection ClientConnection_t;
+
+Endpoint_t* krs_endpoint_setup(Address_t address, Channel_t max_channel);
 
 #endif //KRONOS_SERVER_H
