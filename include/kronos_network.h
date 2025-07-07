171bb738f1f62784c3229c13e9f6eafc59138239
saveligulas <saveli.gulas@gmail.com>
2025-07-07 23:51:23 +0200

    added tyedef for SocketRef and Address, used windows socket implementation for now

--- include/kronos_network.h
+++ include/kronos_network.h
@@ -1,10 +1,13 @@
 #ifndef KRONOS_NETWORK_H
 #define KRONOS_NETWORK_H
 #include <stdint.h>
+#include <winsock2.h>
 
+typedef SOCKET UDPSocketRef_t;
 typedef struct Endpoint Endpoint_t;
 typedef uint8_t Channel_t;
 typedef struct Connection Connection_t;
+typedef struct sockaddr_in6 Address_t;
 
 enum ChannelType {
     MESSAGE_CHANNEL,
