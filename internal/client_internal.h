8ec11b6844296e6b78006e38ab793c3e99439183
saveligulas <saveli.gulas@gmail.com>
2025-07-07 23:52:14 +0200

    added emtpy client and server internal headers with structs

---
+++ internal/client_internal.h
@@ -0,0 +1,8 @@
+#ifndef CLIENT_INTERNAL_H
+#define CLIENT_INTERNAL_H
+#include "kronos_client.h"
+
+struct ServerConnection {
+};
+
+#endif //CLIENT_INTERNAL_H
---
+++ internal/server_internal.h
@@ -0,0 +1,12 @@
+#ifndef SERVER_INTERNAL_H
+#define SERVER_INTERNAL_H
+
+struct EndpointManager {
+
+};
+
+struct ClientConnection {
+
+};
+
+#endif //SERVER_INTERNAL_H
