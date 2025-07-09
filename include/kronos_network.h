e317842907815b4ea55b4d2a5d4c9d77dcaeb7fb
saveligulas <saveli.gulas@gmail.com>
2025-07-09 12:49:42 +0200

    decomposed Address into Port and Address with a composite PortAddress, added definitions for max channel and max port numbers

--- include/kronos_network.h
+++ include/kronos_network.h
@@ -2,16 +2,22 @@
 #define KRONOS_NETWORK_H
 #include <stdint.h>
 #include <winsock2.h>
+#include <ws2ipdef.h>
 
 typedef SOCKET UDPSocketRef_t;
 typedef struct Endpoint Endpoint_t;
 typedef uint8_t Channel_t;
 typedef struct Connection Connection_t;
-typedef struct sockaddr_in6 Address_t;
+typedef struct sockaddr_in6 PortAddress_t;
+typedef struct in6_addr Address_t;
+typedef uint16_t Port_t;
 
 enum ChannelType {
     MESSAGE_CHANNEL,
     SOCKET_CHANNEL
 };
+
+#define MAX_CHANNEL_NUMBER 255;
+#define MAX_PORT_NUMBER 65535;
 
 #endif //KRONOS_NETWORK_H
