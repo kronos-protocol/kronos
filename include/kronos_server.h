5f9c72d90f8b51653aad31eb1f20614f96f14bc7
saveligulas <saveli.gulas@gmail.com>
2025-07-09 14:48:09 +0200

    changed EndpointManager to ServerPortManager and added new function definitions for ServerPortManager

--- include/kronos_server.h
+++ include/kronos_server.h
@@ -2,9 +2,11 @@
 #define KRONOS_SERVER_H
 #include "kronos_network.h"
 
-typedef struct EndpointManager EndpointManager_t;
+typedef struct ServerPortManager ServerPortManager_t;
 typedef struct ClientConnection ClientConnection_t;
 
-Endpoint_t* krs_endpoint_setup(Address_t address, Channel_t max_channel);
+ServerPortManager_t* krs_server_port_manager_create(Address_t default_address);
+void krs_server_port_manager_port_add(ServerPortManager_t* server_port_manager, Port_t port);
+void krs_server_port_manager_port_add_with_address(ServerPortManager_t* server_port_manager, Port_t port, Address_t address);
 
 #endif //KRONOS_SERVER_H
