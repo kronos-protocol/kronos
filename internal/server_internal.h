5c727daaf1235296d22f50e51b80a7ce91f6fad3
saveligulas <saveli.gulas@gmail.com>
2025-07-09 14:49:59 +0200

    implemented ServerPortManager and added internal ServerPort struct

--- internal/server_internal.h
+++ internal/server_internal.h
@@ -1,12 +1,25 @@
 #ifndef SERVER_INTERNAL_H
 #define SERVER_INTERNAL_H
+#include "network_internal.h"
 
-struct EndpointManager {
+typedef struct ServerPort ServerPort_t;
 
+struct ServerPortManager {
+    ServerPort_t* open_ports;
+    Port_t open_ports_length;
+    Address_t default_address;
+    Channel_t default_max_channel;
+};
+
+struct ServerPort {
+    Address_t address;
+    Channel_t max_channel;
 };
 
 struct ClientConnection {
 
 };
+
+boolean krs_server_port_manager_validate(ServerPortManager_t* server_port_manager);
 
 #endif //SERVER_INTERNAL_H
