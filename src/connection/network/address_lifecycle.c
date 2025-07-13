41681684f8dee2358c7deb5ccd06453e9ee150af
saveligulas <saveli.gulas@gmail.com>
2025-07-14 00:33:39 +0200

    implemented address_ipv4_create

---
+++ src/connection/network/address_lifecycle.c
@@ -0,0 +1,19 @@
+#include "kronos_network.h"
+#include "network_internal.h"
+
+
+Address_t krs_network_address_ipv4_create(char* ip, const uint8_t ip_length) {
+    if (ip_length > MAX_IPV4_LENGTH) {
+        //TODO: error handling
+        return (Address_t) {0};
+    }
+
+    if (ip[ip_length - 1] != '\0') {
+        return (Address_t) {0};
+    }
+
+    Address_t result;
+
+    inet_pton(AF_INET6, ip, &result);
+
+}
