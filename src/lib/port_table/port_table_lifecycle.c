aff142e3dbefb6a2eadf6f40acccbaf4b5ac0860
Saveli Gulas <saveli.gulas@gmail.com>
2025-07-29 19:07:43 +0200

    added port_table_lifecycle.c with implementation of krs_lib_port_table_create

---
+++ src/lib/port_table/port_table_lifecycle.c
@@ -0,0 +1,14 @@
+#include "kronos_port_table.h"
+#include "port_table_internal.h"
+
+PortTable_t* krs_lib_port_table_create() {
+    PortTable_t* port_table = malloc(sizeof(PortTable_t));
+    uint32_t initial_size = PRIME_SIZES[0];
+    PortLink_t** port_link = malloc(initial_size * sizeof(PortLink_t));
+
+    port_table->table = port_link;
+    port_table->table_size = initial_size;
+    port_table->table_length = 0;
+
+    return port_table;
+}
