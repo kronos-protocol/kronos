37a7213461917ba850449a89281672f463f0140e
saveli <saveli@LAPTOP-SH6L8DA8.home>
2025-08-07 15:32:09 +0200

    added OperationResult as general error handling struct to Error API

--- include/kronos_error.h
+++ include/kronos_error.h
@@ -1,4 +1,13 @@
 #ifndef KRONOS_ERROR_H
 #define KRONOS_ERROR_H
 
+#include <stdbool.h>
+
+typedef struct OperationResult OperationResult_t;
+
+struct OperationResult {
+    bool valid;
+    int error_code;
+};
+
 #endif //KRONOS_ERROR_H
