9c387d4d79adfe4474a756722e56436b9772c79f
saveli <saveli@LAPTOP-SH6L8DA8.home>
2025-08-11 13:13:15 +0200

    added error enum codes

--- include/kronos_error.h
+++ include/kronos_error.h
@@ -4,10 +4,21 @@
 #include <stdbool.h>
 
 typedef struct OperationResult OperationResult_t;
+typedef enum KronosError KronosError_e;
 
 struct OperationResult {
     bool valid;
     int error_code;
+};
+
+enum KronosError {
+    KRS_ERR_SUCCESS = 0,
+
+    // General Errors(1-99)
+    KRS_INVALID_ARGUMENT = 1,
+    KRS_ERR_NULL_POINTER = 2,
+    KRS_ERR_MEMORY_ALLOCATION = 3,
+    KRS_ERR_BUFFER_TOO_SMALL = 4
 };
 
 #endif //KRONOS_ERROR_H
