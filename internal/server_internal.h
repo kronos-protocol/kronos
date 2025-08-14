07b81c46826366e989615bf3efbfbfd8d58d7f5a
saveli <saveli@LAPTOP-SH6L8DA8.home>
2025-08-14 16:50:31 +0200

    fixed naming of port managing scheme to be more understandable, Manager: multiple children - Handler: handles one thing

--- internal/server_internal.h
+++ internal/server_internal.h
@@ -22,12 +22,12 @@ struct ClientConnectionInfo {
     struct tm timestamp_opened;
 };
 
-struct SocketManager {
+struct SocketHandler {
     UDPSocketRef_t udp_socket_ref;
     ChannelHandler_t channel_handlers[];
 };
 
-struct ChannelHandler {
+struct ChannelManager {
     ClientConnectionInfo_t client_connection_info;
     enum ChannelType channel_type;
 };
