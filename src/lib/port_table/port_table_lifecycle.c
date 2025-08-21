90fecb72801366464b607e52bb1d28374aa40564
Saveli Gulas <saveli.gulas@gmail.com>
2025-08-21 17:24:26 +0200

    fixed port table creation to use correct allocation of memory

--- src/lib/port_table/port_table_lifecycle.c
+++ src/lib/port_table/port_table_lifecycle.c
@@ -4,10 +4,13 @@
 PortTable_t* krs_lib_port_table_create() {
     PortTable_t* port_table = malloc(sizeof(PortTable_t));
     uint32_t initial_size = PRIME_SIZES[0];
-    PortLink_t** port_link = malloc(initial_size * sizeof(PortLink_t));
+    PortLink_t** port_link = malloc(initial_size * sizeof(PortLink_t*));
+
+    for (uint32_t i = 0; i < initial_size; i++) {
+        port_link[i] = NULL;
+    }
 
     port_table->table = port_link;
     port_table->table_size = initial_size;
-
     return port_table;
 }
