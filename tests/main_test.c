3c27b83f9e23549b75287049bed5fc72f368e42e
Saveli Gulas <saveli.gulas@gmail.com>
2025-11-21 11:21:54 +0100

    add new test to main_test.c

--- tests/main_test.c
+++ tests/main_test.c
@@ -6,6 +6,7 @@ void tearDown() {}
 
 void test_port_table_internal_prime_sizes_exist(void);
 void test_port_table_creation(void);
+void test_port_table_insert(void);
 
 
 void main() {
@@ -13,6 +14,7 @@ void main() {
 
     RUN_TEST(test_port_table_internal_prime_sizes_exist);
     RUN_TEST(test_port_table_creation);
+    RUN_TEST(test_port_table_insert);
 
     UNITY_END();
 }
