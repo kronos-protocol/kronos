86bf419cefb5c1b098643eb6146051f403d7eefa
Saveli Gulas <saveli.gulas@gmail.com>
2025-11-24 16:30:58 +0100

    add error handling to existing function declarations

--- include/kronos_port_table.h
+++ include/kronos_port_table.h
@@ -2,21 +2,27 @@
 #define KRONOS_PORT_TABLE_H
 
 #include "kronos_network.h"
+#include "kronos_server.h"
+#include "kronos_error.h"
+
 #include <stdlib.h>
 #include <stdbool.h>
 
 typedef struct PortTable PortTable_t;
-typedef struct PortTableInsertResult PortTableInsertResult_t;
+typedef struct PortTableCreateResult PortTableCreate_r;
 
-struct PortTableInsertResult {
-    bool valid;
-    int error_code;
+typedef void (*socket_handler_destroy_f) (SocketHandler_t** socket_handler);
+
+struct PortTableCreateResult {
+    KronosResult_b base;
+    PortTable_t* port_table;
 };
 
 PortTable_t* krs_lib_port_table_create();
+PortTableCreate_r krs_lib_port_table_create_s();
 
 void krs_lib_port_table_insert(PortTable_t* port_table, Port_t port);
-PortTableInsertResult_t krs_lib_port_table_insert_s(PortTable_t* port_table, Port_t port);
+Void_r krs_lib_port_table_insert_s(PortTable_t* port_table, Port_t port);
 
 void krs_lib_port_table_destroy(PortTable_t** port_table);
 
