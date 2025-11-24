5e359ac1c1ac8702c0efffcb187ff657b5a4c747
Saveli Gulas <saveli.gulas@gmail.com>
2025-11-24 16:30:27 +0100

    refactor naming scheme of errors

--- include/kronos_error.h
+++ include/kronos_error.h
@@ -3,13 +3,9 @@
 
 #include <stdbool.h>
 
-typedef struct VoidResult VoidResult_t;
+typedef struct VoidResult Void_r;
+typedef struct KronosResultBase KronosResult_b;
 typedef enum KronosError KronosError_e;
-
-struct VoidResult {
-    bool valid;
-    int error_code;
-};
 
 enum KronosError {
     KRS_ERR_SUCCESS = 0,
@@ -20,5 +16,37 @@ enum KronosError {
     KRS_ERR_MEMORY_ALLOCATION = 3,
     KRS_ERR_BUFFER_TOO_SMALL = 4
 };
+
+struct KronosResultBase {
+    bool valid;
+    KronosError_e error_code;
+    const char* error_message;
+    bool free_error_message;
+};
+
+struct VoidResult {
+    KronosResult_b base;
+};
+
+
+static inline
+KronosResult_b krs_lib_error_result_base_suc() {
+    KronosResult_b result;
+    result.valid = true;
+    result.error_code = KRS_ERR_SUCCESS;
+    result.error_message = NULL;
+    result.free_error_message = false;
+    return result;
+}
+
+static inline
+KronosResult_b krs_lib_error_result_base_w_msg(const KronosError_e error_code, const char* error_message) {
+    KronosResult_b result_base;
+    result_base.valid = false;
+    result_base.error_code = error_code;
+    result_base.error_message = error_message;
+    result_base.free_error_message = true;
+    return result_base;
+}
 
 #endif //KRONOS_ERROR_H
