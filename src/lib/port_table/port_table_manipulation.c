fbefecb8e6270534562f8c5bc39da9aad79e25e4
Saveli Gulas <saveli.gulas@gmail.com>
2025-12-03 02:04:02 +0100

    add if checks to functions

--- src/lib/port_table/port_table_manipulation.c
+++ src/lib/port_table/port_table_manipulation.c
@@ -1,11 +1,15 @@
 #include "kronos_port_table.h"
 #include "port_table_internal.h"
 
-bool port_table_insert(PortTable_t* port_table, Port_t port) {
+static bool port_table_insert(PortTable_t* port_table, Port_t port, UdpSocketHandler_t* udp_socket_handler) {
+    if (!port_table || !udp_socket_handler) return false;
+
     size_t index = HASH_PORT_INDEX(port, port_table->table_size);
     PortLink_t* port_link = malloc(sizeof(PortLink_t));
+    if (!port_link) return false;
+
     port_link->port = port;
-    //TODO: initialize SocketHandler for Port
+    port_link->socket_handler = udp_socket_handler;
     port_link->next = NULL;
 
     port_table->total_entries++;
@@ -33,18 +37,24 @@ bool port_table_insert(PortTable_t* port_table, Port_t port) {
     return true;
 }
 
-void krs_lib_port_table_insert(PortTable_t* port_table, Port_t port) {
+void krs_lib_port_table_insert(PortTable_t* port_table, Port_t port, UdpSocketHandler_t* udp_socket_handler) {
+    if (!port_table || !udp_socket_handler) return;
+
+    if (!port_table->table) return;
+
     //TODO: rebuild is called even if the port already exists
     if ((double)port_table->total_entries / port_table->table_size > MAX_LOAD_FACTOR) {
         port_table_rebuild(port_table);
-        port_table_insert(port_table, port);
+        port_table_insert(port_table, port, udp_socket_handler);
         return;
     }
 
-    port_table_insert(port_table, port);
+    port_table_insert(port_table, port, udp_socket_handler);
 }
 
 void port_table_rebuild(PortTable_t* port_table) {
+    if (port_table == NULL) return;
+
     PortLink_t** old_port_links = port_table->table;
     uint32_t old_table_size = port_table->table_size;
     uint8_t next_index = port_table->prime_size_index + 1;
@@ -55,6 +65,9 @@ void port_table_rebuild(PortTable_t* port_table) {
 
     uint32_t new_table_size = PRIME_SIZES[next_index];
     PortLink_t** new_table = calloc(new_table_size, sizeof(PortLink_t*));
+
+    if (new_table == NULL) return;
+
     port_table->table = new_table;
     port_table->table_size = new_table_size;
     port_table->prime_size_index = next_index;
@@ -64,7 +77,7 @@ void port_table_rebuild(PortTable_t* port_table) {
         PortLink_t* current_port_link = old_port_links[i];
 
         while (current_port_link != NULL) {
-            port_table_insert(port_table, current_port_link->port);
+            port_table_insert(port_table, current_port_link->port, current_port_link->socket_handler);
             PortLink_t* removable_port_link = current_port_link;
             current_port_link = current_port_link->next;
             free(removable_port_link);
