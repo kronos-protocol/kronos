30d1fc8644f59b8e3c56341ad527684f5ba5d2f3
Saveli Gulas <saveli.gulas@gmail.com>
2025-12-03 02:04:29 +0100

    update PortTable tests to use new insert function

--- test/port_table_test.c
+++ test/port_table_test.c
@@ -1,15 +1,14 @@
 #include "malloc_wrapper.h"
 #include "unity.h"
 #include "mock_kronos_server.h"
+#include "server_internal.h"
+#include "mock_kronos_port_table.h"
 
 
 #include <kronos_port_table.h>
 #include <port_table_internal.h>
 
-static void mock_socket_handler_destroy() {
-    krs_server_socket_handler_destroy_Ignore();
-}
-
+static UdpSocketHandler_t udp_socket_handler;
 
 void test_port_table_internal_prime_sizes_exist(void) {
     TEST_ASSERT_TRUE(PRIME_SIZES_COUNT > 0);
@@ -17,7 +16,7 @@ void test_port_table_internal_prime_sizes_exist(void) {
 
 void test_port_table_creation(void) {
     mock_malloc_fail_next();
-    mock_socket_handler_destroy();
+    krs_server_udp_socket_handler_destroy_Ignore();
 
     PortTable_t* pt_empty = krs_lib_port_table_create();
     TEST_ASSERT_NULL(pt_empty);
@@ -38,32 +37,34 @@ void test_port_table_creation(void) {
 }
 
 void test_port_table_insert(void) {
-    mock_socket_handler_destroy();
+    krs_server_udp_socket_handler_destroy_Ignore();
 
-    PortTable_t* pt = krs_lib_port_table_create();
-    TEST_ASSERT_NOT_NULL(pt);
-    TEST_ASSERT_NOT_NULL(pt->table);
+    PortAddress_t dummy_port_address = {0};
+    PortLink_t* port_link_arr[1] = {NULL};
+    PortLink_t** port_linkpp = port_link_arr;
+    PortTable_t port_table = {.table = port_linkpp, .table_size = 1, .prime_size_index = -1, .total_entries = 0};
+    PortTable_t* pt = &port_table;
 
     Port_t port = 10;
     uint32_t expected_index = HASH_PORT_INDEX(port, pt->table_size);
 
     TEST_ASSERT_NULL(pt->table[expected_index]);
 
-    krs_lib_port_table_insert(pt, port);
+    krs_lib_port_table_insert(pt, port, krs_server_udp_socket_handler_create(dummy_port_address));
 
     TEST_ASSERT_NOT_NULL(pt->table[expected_index]);
     TEST_ASSERT_EQUAL_UINT32(1, pt->total_entries);
     TEST_ASSERT_EQUAL(pt->table[expected_index]->port, port);
     TEST_ASSERT_NULL(pt->table[expected_index]->next);
 
-    krs_lib_port_table_insert(pt, port);
+    krs_lib_port_table_insert(pt, port, krs_server_udp_socket_handler_create(dummy_port_address));
     TEST_ASSERT_EQUAL_UINT32(1, pt->total_entries);
 
     krs_lib_port_table_destroy(&pt);
 }
 
 void test_port_table_destroy(void) {
-    mock_socket_handler_destroy();
+    krs_server_udp_socket_handler_destroy_Ignore();
 
     PortTable_t* pt = krs_lib_port_table_create();
     TEST_ASSERT_NOT_NULL(pt);
