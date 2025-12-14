f96a22ae43c074ff594ebdc90b781333837c94e0
Saveli Gulas <saveli.gulas@gmail.com>
2025-12-14 23:43:02 +0100

    use new hash_port_index function

--- src/lib/port_table/port_table_manipulation.c
+++ src/lib/port_table/port_table_manipulation.c
@@ -4,7 +4,7 @@
 static bool port_table_insert(PortTable_t* port_table, Port_t port, UdpSocketHandler_t* udp_socket_handler) {
     if (!port_table || !udp_socket_handler) return false;
 
-    size_t index = HASH_PORT_INDEX(port, port_table->table_size);
+    size_t index = hash_port_index(port, port_table->table_size);
     PortLink_t* port_link = malloc(sizeof(PortLink_t));
     if (!port_link) return false;
 
