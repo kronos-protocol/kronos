a63fcd835c5a139b22b54c2d4dfc50ebf4de892b
Saveli Gulas <saveli.gulas@gmail.com>
2026-02-15 00:21:00 +0100

    add result types to header and rename UDPSocketHandler to UDPSocketDescriptor

--- include/kronos_server.h
+++ include/kronos_server.h
@@ -3,15 +3,18 @@
 #include "kronos_network.h"
 
 typedef struct ServerPortManager ServerPortManager_t;
-typedef struct UdpSocketHandler UdpSocketHandler_t;
+typedef struct UDPSocketDescriptor UDPSocketDescriptor_t;
 typedef struct ChannelHandler ChannelHandler_t;
 typedef struct ClientConnection ClientConnection_t;
 
 ServerPortManager_t* krs_server_port_manager_create(Address_t default_address);
-void krs_server_port_manager_port_add(ServerPortManager_t* server_port_manager, Port_t port);
-void krs_server_port_manager_port_add_with_address(ServerPortManager_t* server_port_manager, Port_t port, Address_t address);
 
-UdpSocketHandler_t* krs_server_udp_socket_handler_create(PortAddress_t port_address);
-void krs_server_udp_socket_handler_destroy(UdpSocketHandler_t** socket_handler);
+void krs_server_port_manager_port_add(ServerPortManager_t* spm, Port_t port);
+void krs_server_port_manager_port_add_with_address(ServerPortManager_t* spm, Port_t port, Address_t address);
+
+void krs_server_port_manager_handle_message(ServerPortManager_t* spm, uint8_t* message, size_t size);
+
+UDPSocketDescriptor_t* krs_server_udp_socket_handler_create(PortAddress_t port_address);
+void krs_server_udp_socket_handler_destroy(UDPSocketDescriptor_t** socket_handler);
 
 #endif //KRONOS_SERVER_H
