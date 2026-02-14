be34530fdddb304e61b40bfe173c2c31bbd8f68c
Saveli Gulas <saveli.gulas@gmail.com>
2026-02-15 00:20:51 +0100

    add result types to header and rename UDPSocketHandler to UDPSocketDescriptor

--- include/kronos_network.h
+++ include/kronos_network.h
@@ -1,5 +1,6 @@
 #ifndef KRONOS_NETWORK_H
 #define KRONOS_NETWORK_H
+#include "kronos_error.h"
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -25,15 +26,21 @@ typedef uint8_t Channel_t;
 typedef struct Connection Connection_t;
 typedef struct sockaddr_in6 PortAddress_t;
 typedef struct in6_addr Address_t;
-typedef struct AddressResult AddressResult_t;
+typedef struct AddressCreateResult AddressCreate_r;
 typedef uint16_t Port_t;
+typedef struct UDPSocketRefPort UDPSocketRefPort_t;
+typedef struct UDPThreadedSocketHandler UDPThreadedSocketHandler_t;
 
 typedef enum ChannelType ChannelType_e;
 
-struct AddressResult {
+struct UDPSocketRefPort {
+    UDPSocketRef_t socket;
+    int port;
+};
+
+struct AddressCreateResult {
+    KronosResult_b base;
     Address_t address;
-    bool valid;
-    int error_code;
 };
 
 enum ChannelType {
@@ -43,15 +50,19 @@ enum ChannelType {
 };
 
 Address_t krs_network_address_ipv4_create(const char* ip);
-AddressResult_t krs_network_address_ipv4_create_s(const char* ip);
+AddressCreate_r krs_network_address_ipv4_create_s(const char* ip);
 Address_t krs_network_address_ipv4_create_len(const char* ip, const uint8_t ip_length);
-AddressResult_t krs_network_address_ipv4_create_len_s(const char* ip, const uint8_t ip_length);
+AddressCreate_r krs_network_address_ipv4_create_len_s(const char* ip, const uint8_t ip_length);
 
 Address_t krs_network_address_ipv6_create(char* ip, uint8_t ip_length);
 
 PortAddress_t krs_network_port_address_create(Port_t port, Address_t address);
 
 UDPSocketRef_t krs_network_udp_socket_ref_create(PortAddress_t port_address);
+
+UDPThreadedSocketHandler_t* krs_network_udp_socket_handler_threaded_create(int thread_count);
+Void_r krs_network_udp_socket_handler_threaded_handle_add(UDPThreadedSocketHandler_t* udp_tsh, UDPSocketRef_t* udp_sr);
+void krs_network_udp_socket_handler_threaded_destroy(UDPThreadedSocketHandler_t** udp_tsh, bool destroy_socket_refs);
 
 #define MAX_CHANNEL_NUMBER 255
 #define MAX_PORT_NUMBER 65535
--- include/kronos_port_table.h
+++ include/kronos_port_table.h
@@ -8,21 +8,32 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define CMP_ID "LIB-PORT-TABLE"
+
 typedef struct PortTable PortTable_t;
 typedef struct PortTableCreateResult PortTableCreate_r;
+typedef struct PortTableLookupResult PortTableLookup_r;
 
-typedef void (*SocketHandlerDestroy_f) (UdpSocketHandler_t** socket_handler);
+typedef void (*SocketHandlerDestroy_f) (UDPSocketDescriptor** socket_handler);
 
 struct PortTableCreateResult {
     KronosResult_b base;
     PortTable_t* port_table;
 };
 
+struct PortTableLookupResult {
+    KronosResult_b base;
+    bool exists;
+    UDPSocketDescriptor* socket_handler;
+};
+
 PortTable_t* krs_lib_port_table_create();
 PortTableCreate_r krs_lib_port_table_create_s();
 
-void krs_lib_port_table_insert(PortTable_t* port_table, Port_t port, UdpSocketHandler_t* udp_socket_handler);
-Void_r krs_lib_port_table_insert_s(PortTable_t* port_table, Port_t port, UdpSocketHandler_t* udp_socket_handler);
+void krs_lib_port_table_insert(PortTable_t* port_table, Port_t port, UDPSocketDescriptor* udp_socket_handler);
+Void_r krs_lib_port_table_insert_s(PortTable_t* port_table, Port_t port, UDPSocketDescriptor* udp_socket_handler);
+
+PortTableLookup_r krs_lib_port_table_lookup(PortTable_t* port_table, Port_t port);
 
 void krs_lib_port_table_destroy(PortTable_t** port_table);
 
