b20f18c39d660baa7cc68ae533fc4abc329c0c4e
Saveli Gulas <saveli.gulas@gmail.com>
2026-03-19 14:50:50 +0100

    use new error codes

--- src/lib/port_table/port_table_interaction.c
+++ src/lib/port_table/port_table_interaction.c
@@ -1,20 +1,18 @@
 #include "kronos_log.h"
-
-
-#import <kronos_port_table.h>
-#import <port_table_internal.h>
+#include "kronos_port_table.h"
+#include "port_table_internal.h"
 
 
 PortTableLookup_r krs_lib_port_table_lookup(PortTable_t* port_table, Port_t port) {
     PortTableLookup_r result;
     result.exists = false;
     result.socket_handler = NULL;
-    result.base.error_code = KRS_ERR_SUCCESS;
+    result.base.error_code = KRS_SUCCESS;
 
     if (!port_table) {
         char* msg = "port_table is NULL";
         KRS_LOG_ERROR(CMP_ID, "%s: { %u }", msg, port_table);
-        result.base = krs_lib_error_result_base_w_msg(KRS_INVALID_ARGUMENT, "%s", msg);
+        result.base = krs_lib_error_result_base_w_msg(KRS_ERR_NULL_POINTER, "%s", msg);
         return result;
     }
 
