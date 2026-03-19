dd69870eaeb33c3834b38a046c8bc485738882d9
Saveli Gulas <saveli.gulas@gmail.com>
2026-03-19 14:25:24 +0100

    add ServerConnection body

--- internal/client_internal.h
+++ internal/client_internal.h
@@ -1,8 +1,17 @@
 #ifndef CLIENT_INTERNAL_H
 #define CLIENT_INTERNAL_H
+
 #include "kronos_client.h"
+#include "kronos_network.h"
+
+#include <stdbool.h>
+#include <stdint.h>
 
 struct ServerConnection {
+    UDPSocketRef_t socket;
+    PortAddress_t  server_address;
+    uint32_t       connection_id;
+    bool           connected;
 };
 
-#endif //CLIENT_INTERNAL_H
+#endif // CLIENT_INTERNAL_H
