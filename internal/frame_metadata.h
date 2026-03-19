4fe2bfb9ff740a658c6b5498dfb035fe37dbd936
Saveli Gulas <saveli.gulas@gmail.com>
2026-03-19 14:31:36 +0100

    add MetadataFlagPosition_e content and size array

--- internal/frame_metadata.h
+++ internal/frame_metadata.h
@@ -3,11 +3,16 @@
 #include <stdint.h>
 
 typedef enum {
-    FLAG_COUNT
-} MetadataFlagPosition;
+    META_FLAG_ACK_REQUIRED = 0,
+    META_FLAG_FRAGMENT_INFO = 1,
+    META_FLAG_ACK_ID       = 2,
+    META_FLAG_PRIORITY     = 3,
+    META_FLAG_TIMESTAMP    = 4,
+    META_FLAG_COUNT
+} MetadataFlagPosition_e;
 
-static const uint8_t KRS_METADATA_FLAG_POSITION_SIZE[] ={
-
+static const uint8_t KRS_METADATA_FLAG_POSITION_SIZE[] = {
+    0, 4, 8, 1, 8
 };
 
 #endif //FRAME_METADATA_H
