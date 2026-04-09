e09993e6a0b2c5d816be8085e1a44840379e7ac3
Saveli Gulas <saveli.gulas@gmail.com>
2026-04-09 14:19:56 +0200

    cleanup imports and obsolete TODOs

--- src/frame/frame_content.c
+++ src/frame/frame_content.c
@@ -1,16 +1,14 @@
 #include "kronos.h"
-#include "../../internal/frame_body.h"
+#include "frame_body.h"
 
 #include <string.h>
 
 uint16_t krs_frame_get_content(const Frame_t* frame, uint8_t* out, uint16_t out_data_size) {
     if (frame == NULL) {
-        //TODO: handle error with errno
         return 0;
     }
 
     if (frame->body_length > out_data_size) {
-        //TODO: same as above
         return 0;
     }
 
