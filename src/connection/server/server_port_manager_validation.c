a0708808137edd8b8ca58b5efb1de6423f94ae32
Saveli Gulas <saveli.gulas@gmail.com>
2026-04-17 16:34:08 +0200

    implement krs_server_port_manager_validate with basic logic

--- src/connection/server/server_port_manager_validation.c
+++ src/connection/server/server_port_manager_validation.c
@@ -1,1 +1,9 @@
+#include "kronos_server.h"
+#include "server_internal.h"
 
+
+bool krs_server_port_manager_validate(ServerPortManager_t* server_port_manager) {
+    if (!server_port_manager) return false;
+    if (!server_port_manager->port_table) return false;
+    return true;
+}
