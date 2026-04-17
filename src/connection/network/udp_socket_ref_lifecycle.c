0d8ea2d924d0c535967e92e7334741c767a3a24f
Saveli Gulas <saveli.gulas@gmail.com>
2026-04-17 16:35:24 +0200

    update krs_network_udp_socket_ref_create to be more robust on windows

--- src/connection/network/udp_socket_ref_lifecycle.c
+++ src/connection/network/udp_socket_ref_lifecycle.c
@@ -1,6 +1,11 @@
-#include <kronos_server.h>
-#include <server_internal.h>
+#include <kronos_network.h>
+#include <network_internal.h>
 
+#include <mswsock.h>
+
+#ifndef SIO_UDP_CONNRESET
+#define SIO_UDP_CONNRESET _WSAIOW(IOC_VENDOR, 12)
+#endif
 
 UDPSocketRef_t krs_network_udp_socket_ref_create(PortAddress_t port_address) {
     struct sockaddr_in6 addr = port_address;
@@ -12,10 +17,16 @@ UDPSocketRef_t krs_network_udp_socket_ref_create(PortAddress_t port_address) {
     DWORD off = 0;
     setsockopt(s, IPPROTO_IPV6, IPV6_V6ONLY, (const char*)&off, sizeof(off));
 
-    if (bind(s, addr, sizeof(addr)) == SOCKET_ERROR) {
+    BOOL connreset_disabled = FALSE;
+    DWORD bytes_returned = 0;
+    WSAIoctl(s, SIO_UDP_CONNRESET, &connreset_disabled, sizeof(connreset_disabled),
+             NULL, 0, &bytes_returned, NULL, NULL);
+
+    if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
         closesocket(s);
         return INVALID_SOCKET;
     }
 
     return s;
 }
+
