9f41b78d9b5382ece188b3fa03ba17513c2e8857
Saveli Gulas <saveli.gulas@gmail.com>
2026-04-17 16:35:39 +0200

    remove unused TODO

--- src/math/uint_conversion.c
+++ src/math/uint_conversion.c
@@ -1,12 +1,13 @@
-#include <stdint.h>
 #include "kronos_math.h"
+
+#include <stdint.h>
 
 uint16_t krs_math_uint16_from_uint8(const uint8_t ms_byte, const uint8_t ls_byte) {
     return ((uint16_t)ms_byte << 8) | ls_byte;
 }
 
 uint64_t krs_math_uint64_from_uint8(const uint8_t* source, const uint8_t offset, const uint16_t source_length) {
-    if (offset + 8 > source_length) return 0; //TODO: proper error handling
+    if (offset + 8 > source_length) return 0;
 
     uint64_t result = 0;
     for (int i = 0; i < 8; i++) {
