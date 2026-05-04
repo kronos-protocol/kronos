fd9be81b5d43319fd800bf5a85beb1a9649b3316
Saveli Gulas <saveli.gulas@gmail.com>
2026-05-05 01:15:33 +0200

    add error handling to udp_socket_ref_lifecycle.c

--- src/connection/network/udp_socket_ref_lifecycle.c
+++ src/connection/network/udp_socket_ref_lifecycle.c
@@ -1,4 +1,5 @@
 #include <kronos_network.h>
+#include <kronos_log.h>
 #include <network_internal.h>
 
 #include <mswsock.h>
@@ -9,10 +10,17 @@
 
 UDPSocketRef_t krs_network_udp_socket_ref_create(PortAddress_t port_address) {
     struct sockaddr_in6 addr = port_address;
-    if (addr.sin6_family != AF_INET6) return INVALID_SOCKET;
+    if (addr.sin6_family != AF_INET6) {
+        KRS_LOG_ERROR("udp_socket", "invalid address family %d (expected AF_INET6)",
+                      addr.sin6_family);
+        return INVALID_SOCKET;
+    }
 
     SOCKET s = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
-    if (s == INVALID_SOCKET) return INVALID_SOCKET;
+    if (s == INVALID_SOCKET) {
+        KRS_LOG_ERROR("udp_socket", "socket() failed (err=%d)", WSAGetLastError());
+        return INVALID_SOCKET;
+    }
 
     DWORD off = 0;
     setsockopt(s, IPPROTO_IPV6, IPV6_V6ONLY, (const char*)&off, sizeof(off));
@@ -23,6 +31,7 @@ UDPSocketRef_t krs_network_udp_socket_ref_create(PortAddress_t port_address) {
              NULL, 0, &bytes_returned, NULL, NULL);
 
     if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
+        KRS_LOG_ERROR("udp_socket", "bind() failed (err=%d)", WSAGetLastError());
         closesocket(s);
         return INVALID_SOCKET;
     }
