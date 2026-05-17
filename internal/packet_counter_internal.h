ac6d334af40608d300feb7e6b426faf288acf14c
Saveli Gulas <saveli.gulas@gmail.com>
2026-05-17 20:15:14 +0200

    change counters to volatile LONG64[]

--- internal/packet_counter_internal.h
+++ internal/packet_counter_internal.h
@@ -3,8 +3,10 @@
 
 #include "kronos_packet_counter.h"
 
+#include <winsock2.h>
+
 struct PacketCounter {
-    uint64_t counters[MAX_CHANNEL_NUMBER + 1];
+    volatile LONG64 counters[MAX_CHANNEL_NUMBER + 1];
 };
 
 #endif // PACKET_COUNTER_INTERNAL_H
