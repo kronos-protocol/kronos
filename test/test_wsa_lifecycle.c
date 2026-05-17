577f5c8e97bf5bec008151b52c517d4f9a0366a0
Saveli Gulas <saveli.gulas@gmail.com>
2026-05-17 20:34:53 +0200

    add stress test in test_wsa_lifecycle.c

--- test/test_wsa_lifecycle.c
+++ test/test_wsa_lifecycle.c
@@ -3,7 +3,9 @@
 #include "malloc_wrapper.h"
 
 #include <unity.h>
+#include <stdint.h>
 #include <winsock2.h>
+#include <windows.h>
 
 
 void test_wsa_init_cleanup_single(void) {
@@ -39,6 +41,40 @@ void test_wsa_cleanup_without_init(void) {
     TEST_ASSERT_NOT_EQUAL(INVALID_SOCKET, s);
     closesocket(s);
 
+    krs_wsa_cleanup();
+}
+
+typedef struct WsaStressArgs {
+    uint32_t iterations;
+} WsaStressArgs_t;
+
+static DWORD WINAPI s_wsa_init_cleanup_thread(LPVOID param) {
+    WsaStressArgs_t* args = (WsaStressArgs_t*)param;
+    for (uint32_t i = 0; i < args->iterations; i++) {
+        krs_wsa_init();
+        SOCKET s = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+        if (s != INVALID_SOCKET) closesocket(s);
+        krs_wsa_cleanup();
+    }
+    return 0;
+}
+
+void test_wsa_init_cleanup_concurrent_no_crash(void) {
+    enum { THREAD_COUNT = 16, ITERATIONS_PER_THREAD = 1000 };
+    HANDLE threads[THREAD_COUNT];
+    WsaStressArgs_t args = { .iterations = ITERATIONS_PER_THREAD };
+
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        threads[i] = CreateThread(NULL, 0, s_wsa_init_cleanup_thread, &args, 0, NULL);
+        TEST_ASSERT_NOT_NULL(threads[i]);
+    }
+    WaitForMultipleObjects(THREAD_COUNT, threads, TRUE, INFINITE);
+    for (int i = 0; i < THREAD_COUNT; i++) CloseHandle(threads[i]);
+
+    krs_wsa_init();
+    SOCKET s = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+    TEST_ASSERT_NOT_EQUAL(INVALID_SOCKET, s);
+    closesocket(s);
     krs_wsa_cleanup();
 }
 
