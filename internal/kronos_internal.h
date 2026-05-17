6c95e384f048063443d2ca4bce82d67225d685b2
Saveli Gulas <saveli.gulas@gmail.com>
2026-05-17 20:11:24 +0200

    removed unnecessary comments in Frame struct and add FrameMetadata implementation

--- internal/kronos_internal.h
+++ internal/kronos_internal.h
@@ -2,19 +2,28 @@
 #define KRONOS_INTERNAL_H
 #include <stdint.h>
 
+typedef struct FrameMetadata FrameMetadata_t;
+
+struct FrameMetadata {
+    uint16_t fragment_index;
+    uint16_t fragment_total;
+    uint64_t ack_id;
+    uint8_t priority;
+    uint64_t timestamp_ms;
+};
+
 struct Frame {
-    // Data in Char Buffer
-    // Header
-    char protocol_char;       // must be 0x4B ASCII 'K'
-    uint8_t protocol_version; // bitmask: major(3bit) + minor(3bit) + patch(2bit)
+    char protocol_char;
+    uint8_t protocol_version;
     uint8_t channel;
     uint8_t frame_type;
-    uint16_t presence_flags; // bitmask for flags, each 1bit
+    uint16_t presence_flags;
     uint64_t packet_id;
 
-    // Body
+    FrameMetadata_t metadata;
+
     uint16_t body_length;
-    uint8_t* body; // Contains Metadata from presence_flags and the actual data that is transferred
+    uint8_t* body;
 };
 
 #define KRONOS_FRAME_HEADER_LENGTH 14
