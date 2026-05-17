e8d6b431f13c0b044bd5d7f77af39f11e905cbda
Saveli Gulas <saveli.gulas@gmail.com>
2026-05-17 20:28:27 +0200

    fix code styling

--- src/frame/frame_content.c
+++ src/frame/frame_content.c
@@ -1,19 +1,13 @@
 #include "kronos.h"
-#include "frame_body.h"
 
 #include <string.h>
 
 uint16_t krs_frame_get_content(const Frame_t* frame, uint8_t* out, uint16_t out_data_size) {
-    if (frame == NULL) {
-        return 0;
-    }
+    if (frame == NULL || out == NULL) return 0;
+    if (frame->body_length > out_data_size) return 0;
 
-    if (frame->body_length > out_data_size) {
-        return 0;
+    if (frame->body_length > 0) {
+        memcpy(out, frame->body, frame->body_length);
     }
-
-    int offset = krs_frame_body_metadata_get_length(frame);
-    uint16_t new_size = frame->body_length - offset;
-    memcpy(out, frame->body + offset, frame->body_length - offset);
-    return new_size;
+    return frame->body_length;
 }
