d165f70d6d713a195b72b040ac105e54e103fbe5
Saveli Gulas <saveli.gulas@gmail.com>
2026-05-17 20:05:02 +0200

    add krs_frame_metadata_block_length definition

--- internal/frame_body.h
+++ internal/frame_body.h
@@ -3,6 +3,8 @@
 #include "kronos.h"
 #include <stdint.h>
 
+uint16_t krs_frame_metadata_block_length(uint16_t presence_flags);
+
 uint16_t krs_frame_body_metadata_get_length(const Frame_t* frame);
 
 #endif //FRAME_CONTENT_H
